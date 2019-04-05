bcdedfb5f675fbb635dcbdb9ee54aedd15fec085
CGibeau <claire.gibeau@outlook.com>
2019-04-04 23:21:02 -0700

    added images and formatting

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ https://archnemesisdetector.herokuapp.com/
 https://github.com/citylights521/ArchNemesisDetector
 
 
+## 
+find out who your arch nemesis is! ArchNemesisDetector will compare your survey results to that of others and will return the one from whom you most differ, aka your arch nemesis! While the assignment origionally wanted to find who the user had the most similar results to, this version has flipped that to find who the user has the most different results from.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,24 +3,52 @@ var archNemesis = require("../data/archNemesis.js");
 console.log("api routes loaded");
 
 module.exports = function (app, path, rootPath) {
-    //GET GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-    // Displays all characters
+    // Displays all nemesis'
     app.get("/api/nemesis", function (req, res) {
         return res.json(archNemesis.surveyResults);
 
     });
 
-    //POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+    //this section of code compares the difference between the current user's scores against those from other users, question by question. The differences are added to calculate the total difference. The absolute value of the differences is used.
     app.post("/api/nemesis", function (req, res) {
         var newNemesis = req.body;
         newNemesis.routeName = newNemesis.name.replace(/\s+/g, "").toLowerCase();
 
+        var newNemesisScores = newNemesis.scores;
+
+        var nemIndex = -1;
+        var prevTotalDifference = 0;
+        for (let i = 0; i < archNemesis.surveyResults.length; i++) {
+            const currentNem = archNemesis.surveyResults[i];
+            
+            var currentNemScores = currentNem.scores;
+
+            if (currentNemScores.length != newNemesisScores.length)
+            {
+                throw "Nemesis data is not comprable";
+            }
+
+            //finds the user with the biggest different in survey score (closest match is the user with the biggest amount of difference)
+            var totalDifference = 0;
+            for (let j = 0; j < currentNemScores.length; j++) {
+                const currentNemScore = currentNemScores[j];
+                var currentNewNemScore = newNemesisScores[j];
+
+                totalDifference += Math.abs(currentNemScore - currentNewNemScore);
+            }
+
+            if (totalDifference > prevTotalDifference)
+            {
+                nemIndex = i;
+            }
+
+            prevTotalDifference = totalDifference;
+        }
 
 
-
-
+        //pushes users survey results into the survey results array as can be viewed at /api/nemesis
         archNemesis.surveyResults.push(newNemesis);
-        res.json(newNemesis);
+        res.json(archNemesis.surveyResults[nemIndex]);
     });
 };
 
